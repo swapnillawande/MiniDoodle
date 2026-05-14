c81a8126a35966a5b048c8e97d55f2486362c87a
swapnil <swapnillawande2001@gmail.com>
2026-05-15 00:21:39 +0200

    added getters, setters, and constructors for AppUser entity because Lombok not working

--- src/main/java/com/minidoodle/entity/AppUser.java
+++ src/main/java/com/minidoodle/entity/AppUser.java
@@ -15,11 +15,11 @@ import lombok.Setter;
 
 @Entity
 @Table(name = "users")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
+//@Getter
+//@Setter
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
 public class AppUser {
 	
 	@Id
@@ -32,4 +32,43 @@ public class AppUser {
 	@Column(nullable = false, unique = true)
 	private String email;
 
+	
+	public AppUser() {
+
+	}
+
+	public AppUser(Long id, String username, String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	
+	
 }
