9477a0b5390b7950757031f57e57e3dba8410829
swapnil <swapnillawande2001@gmail.com>
2026-05-14 23:22:14 +0200

    Basic set up for minidoodle app

---
+++ src/main/java/com/minidoodle/MiniDoodleApplication.java
@@ -0,0 +1,13 @@
+package com.minidoodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoodleApplication.class, args);
+	}
+
+}
