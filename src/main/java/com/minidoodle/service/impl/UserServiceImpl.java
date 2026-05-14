3affeccf730a05e8d725c9ebd1e4f0082357e3d1
swapnil <swapnillawande2001@gmail.com>
2026-05-15 00:04:05 +0200

    updated service impl for AppUser entity

--- src/main/java/com/minidoodle/service/impl/UserServiceImpl.java
+++ src/main/java/com/minidoodle/service/impl/UserServiceImpl.java
@@ -1,5 +1,40 @@
 package com.minidoodle.service.impl;
 
-public class UserServiceImpl {
+import java.util.List;
+
+import com.minidoodle.entity.AppUser;
+import com.minidoodle.service.UserService;
+
+public class UserServiceImpl implements UserService{
+
+	@Override
+	public List<AppUser> getAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AppUser getUserById(Long userId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public AppUser addUser() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteUserById(Long userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteAllUsers() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
