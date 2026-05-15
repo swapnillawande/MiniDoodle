c98ddf1de1df7aa8487dc44a915b25d8b0afdd78
swapnil <swapnillawande2001@gmail.com>
2026-05-15 13:25:11 +0200

    fixing TimeSlotRepository and TimeSlotService code mismatch

--- src/main/java/com/minidoodle/repository/TimeSlotRepository.java
+++ src/main/java/com/minidoodle/repository/TimeSlotRepository.java
@@ -1,9 +1,19 @@
 package com.minidoodle.repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.minidoodle.entity.TimeSlot;
 
 public interface TimeSlotRepository extends JpaRepository<TimeSlot, Long>{
 
+    List<TimeSlot> findByOwnerId(Long ownerId);
+
+    List<TimeSlot> findByOwnerIdAndStartTimeLessThanAndEndTimeGreaterThan(
+            Long ownerId,
+            LocalDateTime endTime,
+            LocalDateTime startTime
+    );
 }
--- src/main/java/com/minidoodle/service/impl/TimeSlotServiceImpl.java
+++ src/main/java/com/minidoodle/service/impl/TimeSlotServiceImpl.java
@@ -1,24 +1,9 @@
 package com.minidoodle.service.impl;
 
-import java.time.LocalDateTime;
-import java.util.List;
-
-import com.minidoodle.entity.TimeSlot;
 import com.minidoodle.service.TimeSlotService;
 
 public class TimeSlotServiceImpl implements TimeSlotService{
 
-	@Override
-	public List<TimeSlot> findByOwnerId(Long ownerId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
-	@Override
-	public List<TimeSlot> findByOwnerIdAndStartTimeLessThanAndEndTimeGreaterThan(Long ownerId, LocalDateTime endTime,
-			LocalDateTime startTime) {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
