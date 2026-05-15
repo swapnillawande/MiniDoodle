9f073f87f91a2bbbc32480433568321542a26813
swapnil <swapnillawande2001@gmail.com>
2026-05-15 13:31:34 +0200

    Add TimeSlot DTO and update service methods

--- src/main/java/com/minidoodle/service/TimeSlotService.java
+++ src/main/java/com/minidoodle/service/TimeSlotService.java
@@ -1,7 +1,22 @@
 package com.minidoodle.service;
 
+import java.util.List;
+
+import com.minidoodle.dto.TimeSlotDto;
+import com.minidoodle.entity.enums.SlotStatus;
+
 
 public interface TimeSlotService {
 
+	List<TimeSlotDto> getAllSlots();
 
+	TimeSlotDto getSlotById(Long slotId);
+
+	TimeSlotDto addSlot(TimeSlotDto timeSlotDto);
+
+	TimeSlotDto updateSlotById(Long slotId, TimeSlotDto timeSlotDto);
+
+	void deleteSlotById(Long slotId);
+
+	TimeSlotDto updateSlotStatus(Long slotId, SlotStatus status);
 }
--- src/main/java/com/minidoodle/service/impl/TimeSlotServiceImpl.java
+++ src/main/java/com/minidoodle/service/impl/TimeSlotServiceImpl.java
@@ -1,8 +1,51 @@
 package com.minidoodle.service.impl;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.minidoodle.dto.TimeSlotDto;
+import com.minidoodle.entity.enums.SlotStatus;
 import com.minidoodle.service.TimeSlotService;
 
+@Service
 public class TimeSlotServiceImpl implements TimeSlotService{
+
+	@Override
+	public List<TimeSlotDto> getAllSlots() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeSlotDto getSlotById(Long slotId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeSlotDto addSlot(TimeSlotDto timeSlotDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TimeSlotDto updateSlotById(Long slotId, TimeSlotDto timeSlotDto) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteSlotById(Long slotId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TimeSlotDto updateSlotStatus(Long slotId, SlotStatus status) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 
