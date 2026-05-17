7a5e9a266b349d042f622d04e6dd40ce2cfad497
swapnil <swapnillawande2001@gmail.com>
2026-05-17 11:30:53 +0200

    Added Junit test case for Meeting Entity

---
+++ src/test/java/com/minidoodle/entity/MeetingEntityTest.java
@@ -0,0 +1,36 @@
+package com.minidoodle.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MeetingEntityTest {
+
+    @Test
+    public void testMeetingEntity() {
+
+        Meeting meeting = new Meeting();
+
+        meeting.setId(1L);
+        meeting.setTitle("Full Stack Developer Interview");
+        meeting.setDescription("Interview for java fullstack developer");
+        meeting.setStartTime(LocalDateTime.of(2026, 5, 20, 10, 0));
+        meeting.setEndTime(LocalDateTime.of(2026, 5, 20, 11, 0));
+
+        
+        
+        assertNotNull(meeting);
+
+        assertEquals(1L, meeting.getId());
+        
+        assertEquals("Full Stack Developer Interview", meeting.getTitle());
+        
+        assertEquals("Interview for java fullstack developer", meeting.getDescription());
+    }
+	
+}
