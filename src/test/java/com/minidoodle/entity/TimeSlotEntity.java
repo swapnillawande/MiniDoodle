e592d338d0b1f09e863fae8f92b677d798446f1e
swapnil <swapnillawande2001@gmail.com>
2026-05-17 11:36:24 +0200

    Added Junit test case for TimeSlot Entity

---
+++ src/test/java/com/minidoodle/entity/TimeSlotEntity.java
@@ -0,0 +1,36 @@
+package com.minidoodle.entity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.minidoodle.entity.enums.SlotStatus;
+
+@SpringBootTest
+public class TimeSlotEntity {
+
+	
+	@Test
+	public void testTimeSlotEntity() {
+		
+        TimeSlot timeSlot = new TimeSlot();
+
+        timeSlot.setId(101L);
+        timeSlot.setStartTime(LocalDateTime.of(2026, 5, 28, 14, 0));
+        timeSlot.setEndTime(LocalDateTime.of(2026, 5, 28, 15, 0));
+        timeSlot.setStatus(SlotStatus.FREE);
+
+        
+        
+        assertNotNull(timeSlot);
+
+        assertEquals(101L, timeSlot.getId());
+        
+        assertEquals(SlotStatus.FREE, timeSlot.getStatus());
+		
+	}
+}
